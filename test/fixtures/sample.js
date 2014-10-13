f353e3e6afdf13c62de37a3236f3b3aa091b0ba2
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-10-13 15:10:58 +0200

    Created runJsdoc.js and test

---
+++ test/fixtures/sample.js
@@ -0,0 +1,9 @@
+/**
+ * @postmanName sample
+ * @postmanDescription some words about this sample file
+ * @postmanMethod GET
+ * @postmanUrl http://localhost:8080
+ */
+module.exports = function(req, res) {
+  res.send('hello world');
+};
---
+++ test/test.runJsdoc.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var runJsdoc = require('../lib/runJsdoc');
+
+
+describe('runJsdoc', function() {
+
+  describe('#exec()', function () {
+    it('should return false if jsdoc failed', function () {
+      var result = runJsdoc.exec('./wrong/path');
+      assert.equal(result, false);
+    });
+
+    it('should return true if jsdoc successful exit', function () {
+      var result = runJsdoc.exec('./test/fixtures/sample.js');
+      assert.ok(typeof result === 'object');
+    });
+  });
+
+  describe('#dir()', function () {
+    it('should return false if directory cannot be found', function() {
+       var result = runJsdoc.execAtDir('./wrong/path');
+       assert.equal(result, false);
+     });
+
+     it('should return the jsdoc object', function() {
+       var result = runJsdoc.execAtDir('./test/fixtures');
+       assert.equal(result.length, 1);
+     });
+  });
+
+});
