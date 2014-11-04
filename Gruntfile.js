d8f8c9e2d088c8939a6ab616985f27b1716b4028
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-11-04 17:45:25 +0100

    Added timeout to mocha test

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function (grunt) {
     mochaTest: {
       test: {
         options: {
-          reporter: 'spec'
+          reporter: 'spec',
+          timeout: 5000
         },
         src: ['test/**/*.js']
       }
